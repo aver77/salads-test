69ef753846148d5f3cb04f44ba4404438d03b854
aver77 <dota.nikita@inbox.ru>
2022-01-17 06:37:17 +0300

    remarked

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-import './index.scss';
+import GlobalStyles from './styles/globalStyles';
 
 ReactDOM.render(
     <React.StrictMode>
+        <GlobalStyles/>
         <App />
     </React.StrictMode>,
 document.getElementById('root')
